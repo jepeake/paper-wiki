210d3475b76a87a134529c51fbe80b358a7ffcff
Jacob Peake <jepeake@proton.me>
2024-02-20 19:34:49 +0000

    links

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -12,34 +12,20 @@ const isExternalLink = (url) => /^https?:\/\//.test(url);
 
 const Page = () => {
     return (
-        <>
-            {allLinks.map((link) => {
-                if (isExternalLink(link.url)) {
-                    // Render an <a> tag for external URLs
-                    return (
-                        <a
-                            className="pb-4 flex hover:underline"
-                            href={link.url}
-                            key={link.slug}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <h1 className="overflow-hidden whitespace-nowrap overflow-ellipsis">{link.title}</h1>
-                        </a>
-                    );
-                } else {
-                    // Render a Next.js Link component for internal navigation
-                    return (
-                        <Link href={link.url} key={link.slug}>
-                            <a className="pb-4 flex hover:underline">
-                                <h1 className="overflow-hidden whitespace-nowrap overflow-ellipsis">{link.title}</h1>
-                            </a>
-                        </Link>
-                    );
-                }
-            })}
-        </>
+      <div>
+        {links.filter(link => isExternalLink(link.url)).map((link) => (
+          <a
+            className="pb-4 flex hover:underline"
+            href={link.url}
+            key={link.slug}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h1 className="overflow-hidden whitespace-nowrap overflow-ellipsis">{link.title}</h1>
+          </a>
+        ))}
+      </div>
     );
-};
+  };
 
 export default Page;
